7b68a0e1d844e9a458428019dfab63f1565027ae
Benncio09 <lorebencini13@outlook.com>
2024-09-20 07:51:46 +0200

    esercizio1Tozzi

---
+++ Esercizio1Tozzi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+
+        echo "<h1>Leonardo Tozzi</h1>";
+
+        $peso = 70;
+
+        $tassoIdratazione = $peso * 30;
+
+        echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $tassoIdratazione </p>"
+
+
+    ?>
+</body>
+</html>
